fb04b75a9449ab3335e2d229623d83b765cfdab1
rafroes1 <rafafroes@outlook.com>
2023-03-03 11:20:05 -0300

    Initial Commit

---
+++ CoreComanda/CoreComandaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreComandaApp.swift
+//  CoreComanda
+//
+//  Created by Rafael Carvalho on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreComandaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
